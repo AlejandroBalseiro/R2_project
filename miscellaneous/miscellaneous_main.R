1e9664dbfadaf1bf0edc90efb3c73a293cc980b9
javigilantu <javigilantu@gmailcom>
2021-02-18 18:02:34 +0100

    ML y reshape actualizadas

---
+++ miscellaneous/miscellaneous_main.R
@@ -0,0 +1,18 @@
+# miscellaneous_main
+
+# directory set up
+ 
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+directory <- setwd("..")
+
+print(directory)
+
+lapply(paste0("R/", list.files(path = "R/", recursive = TRUE)), source)
+
+
+# Debugging and unbugging
+debug(main)
+main(directory)
+undebug(main)
